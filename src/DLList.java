a4bceef68614409ae4206e84b2cc48958e300a08
shubhraj <shubhampatil132@gmail.com>
2016-08-03 10:25:00 +0530

    Getter Setter for DLNode Created

--- src/DLList.java
+++ src/DLList.java
@@ -1,6 +1,29 @@
 /**
  * Created by patilsh on 8/3/2016.
  */
-public class DLList {
+public class DLList<T> {
+    private DLNode<T> head ;
+    private DLNode<T> currentNode;
 
+    public DLList() {
+        head = null;
+        currentNode = null;
+    }
+
+    public void insertAtBeginning(){
+        DLNode<T> newNode = new DLNode<T>();
+        if(head == null){
+                head = newNode;
+                head.setNextNode(null);
+            }
+    }
+
+    public void display(){
+
+        currentNode = head;
+        while(currentNode.getNextNode() != null){
+            System.out.println(currentNode.getData());
+        }
+
+    }
 }
